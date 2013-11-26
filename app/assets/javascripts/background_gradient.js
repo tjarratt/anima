1ff403cf99ecac0c5be9cb0cf7e960e169e80380
Tim Jarratt <tjarratt@gmail.com>
2013-11-25 18:32:19 -0800

    BIZNIZ IS SO SERIOUS

--- app/assets/javascripts/background_gradient.js
+++ app/assets/javascripts/background_gradient.js
@@ -16,27 +16,31 @@ Anima.update_background = (function() {
     $('#topGradient').css('opacity', 1 - percentage);
     $('#bottomGradient').addClass('hour-' + next_hour);
 
-    if (date.getHours() == 17) {
-      if (can_haz_serious_business && date.getMinutes() < 30) {
-        from_point = {x: -280};
-        to_point = {x: -20};
-        can_haz_serious_business = false;
-      }
-      else if (date.getMinutes() > 30) {
-        from_point = {x: -20};
-        to_point = {x: -280};
-        can_haz_serious_business = true;
-      }
+    if (can_haz_serious_business && date.getHours() == 17 && date.getMinutes() >= 55) {
+      from_point = {x: -280};
+      to_point = {x: -20};
+      can_haz_serious_business = false;
 
-      if (from_point && to_point) {
-        var bznz = document.getElementById('srsbznz');
-        var tween = new TWEEN.Tween(from_point)
-          .to(to_point, 3000)
-          .easing(TWEEN.Easing.Elastic.InOut)
-          .onUpdate(function() {
-            bznz.setAttribute('style', 'bottom:' + this.x + 'px');
-          }).start();
-      }
+      var bznz = document.getElementById('srsbznz');
+      var tween = new TWEEN.Tween(from_point)
+        .to(to_point, 3000)
+        .easing(TWEEN.Easing.Elastic.InOut)
+        .onUpdate(function() {
+          bznz.setAttribute('style', 'bottom:' + this.x + 'px');
+        }).start();
+    }
+    else if (date.getHours() == 18 && date.getMinutes() >= 30) {
+      to_point = {x: -280};
+      from_point = {x: -20};
+      can_haz_serious_business = false;
+
+      var bznz = document.getElementById('srsbznz');
+      var tween = new TWEEN.Tween(from_point)
+        .to(to_point, 3000)
+        .easing(TWEEN.Easing.Elastic.InOut)
+        .onUpdate(function() {
+          bznz.setAttribute('style', 'bottom:' + this.x + 'px');
+        }).start();
     }
   };
 })();
