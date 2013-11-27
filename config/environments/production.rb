b20663d3e6ee039a8ddaa9be710d5f7c727f09e0
Tim Jarratt <tjarratt@gmail.com>
2013-11-26 18:02:05 -0800

    Clean up required javascriptFix image path for heroku
    Asset pipeline! Argh!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,7 @@ Anima::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += ['serious.png']
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
